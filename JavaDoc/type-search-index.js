e728044b0de4714846f147385301e87dd046fe4f
[Luke Mason] <lmason2@zagmail.gonzaga.edu>
2020-04-15 17:29:32 -0700

    Added Javadoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Account"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CitiusJava"},{"p":"<Unnamed>","l":"ClientList"},{"p":"<Unnamed>","l":"IntroFrame"}]
